f5712dd0e89a6ff7c255631bae52652374cdfe8a
kalle <info@stellarsecurity.com>
2025-02-25 01:37:53 +0100

    Pretty Id Changed.

--- app/Helpers/IdHelper.php
+++ app/Helpers/IdHelper.php
@@ -5,6 +5,12 @@ namespace App\Helpers;
 class IdHelper
 {
 
+    /**
+     * Generates a random string of length 16 - with only numbers.
+     * Typically we use this for Stellar VPN, Antivirus etc, if users dont want,
+     * to use their email.
+     * @return string
+     */
     public static function makePrettyId(): string {
         $characters = '0123456789';
         $charactersLength = strlen($characters);
