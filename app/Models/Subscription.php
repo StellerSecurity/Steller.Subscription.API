6364cc5626eb9a9d626f6e8ee94b3a146f2432c3
Bob <150491881+stellar-security-os@users.noreply.github.com>
2025-03-12 01:50:28 +0100

    Use SoftDeletes.

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Subscription extends Model
 {
 
     use HasUuids;
+    use SoftDeletes;
 
     protected $table = "subscriptions";
 
